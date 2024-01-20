cd1bbc52ae55ab8c33018c34e214d8aecd91802b
Azikk0205 <sevarainomjonova2001@gmail.com>
2024-01-20 16:45:54 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,151 @@
+//1 masala
+
+// let num1 = prompt("1 chi  Sonni kiriting:  ")
+// let num2 = prompt("2 chi  Sonni kiriting:  ")
+
+// if(num1 > num2){
+//     alert(num1)
+// }
+// else{
+//     alert(num2)
+// }
+ 
+
+
+//2 masala
+
+// let num1 = prompt("1 chi  Sonni kiriting:  ")
+// let num2 = prompt("2 chi  Sonni kiriting:  ")
+// let num3 = prompt("3 chi  Sonni kiriting:  ")
+
+// if(num1 > num2 && num1 > num3){
+//     alert(num1)
+// }
+// else if(num3 > num1 && num3 > num2){
+//     alert(num3)
+// }
+// else{
+//     alert(num2)
+// }
+ 
+
+
+//3 masala
+
+// let name1 = prompt("1 chi  Ismini kiriting:  ")
+//let name2 = prompt("2 chi  Ismini kiriting:  ")
+//let name3 = prompt("3 chi  Ismini kiriting:  ")
+
+// if(name1 == "Hoshimjon"){
+//     alert("Xa men shu guruhda o'qiyman")
+// }
+// else if(name1 == "Muhiddin"){
+//     alert("Xa men shu guruhda o'qiyman")
+// }
+// else if(name1 == "Azizbek"){
+//     alert("Xa men shu guruhda o'qiyman")
+// }
+// else{
+//     alert("Yo'q men bu guruhda o'qimayman")
+// }
+ 
+
+
+//4 masala
+
+// let num1 = prompt("1 chi  Sonni kiriting:  ")
+
+// if(num1 > 0){
+//     if(num1 % 2 == 0 ){
+//         alert("Juft  musbat son")
+//     }
+//     else{
+//         alert("Toq musbat son")
+//     }
+// }
+//  else if( num1 < 0 ){
+//     if(num1 % 2 == 0 ){
+//         alert("Manfiy  musbat son")
+//     }
+//     else {
+//         alert("Manfiy toq son")
+//     }
+// }
+
+
+
+// 5 masala
+
+
+// let num1 = prompt("1 dan 9999 gacha Son kiriting:  ")
+
+// if(num1 > 0 && num1 < 9){
+//         alert("Bir xonali son")
+// }
+// if(num1 > 10 && num1 < 99){
+//         alert("Ikki  xonali son")
+// }
+// if(num1 > 100 && num1 < 999){
+//     alert("Uch xonali son")
+// }
+// if (num1 > 1000 && num1 < 9999){
+//     alert("To'rt xonali son")
+// } 
+// else if (num1 > 9999){
+//     alert("Kichikroq son kiriting")
+// }
+
+
+// 6 masala
+
+// let num = prompt("UCh xonali son kiriting:  ")
+// alert( Math.floor(num/100))
+
+
+// 7 masala
+
+// let num1 = prompt("Son kiriting: ")
+// let expr1 = prompt("Ishora kiriting: ")
+// let num2 = prompt("2 chi Son kiriting: ")
+
+// if (expr1 == "+"){
+//     alert(num1+num2)
+// }
+// if (expr1 == "-"){
+//     if (num1 > num2 ){
+//         alert(num1 - num2)
+//     }
+//     else{
+//         alert(num2 - num1)
+//     }
+// }
+// if (expr1 == "*"){
+//     alert(num1 * num2)
+// }
+// if (expr1 == "/"){
+//     if (num2 == 0){
+//         alert("0 ga bo'lib bo'lmaydi")
+//     }
+//     else{
+//         alert(num1/num2)
+//     }
+// }
+
+
+// 8 masala
+
+// let speed = prompt("KM/H ni kiriting: ")
+// let count 
+
+// if (speed < 70){
+//     alert("Shopir hammasi yaxshi")
+// }
+// else {
+//     count = Math.floor((speed - 70)/5)
+//     if (count > 12){
+//     alert("Prava olindi")
+//     }
+//     else{
+//         alert(count)
+//     }
+// }
